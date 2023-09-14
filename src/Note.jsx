0e29265e69a6d2778ecebe907b87821399af0bae
Sabrina Sikder <sabrina.chakma@gmail.com>
2023-09-14 13:13:27 +0200

    remove note state

--- src/Note.jsx
+++ src/Note.jsx
@@ -4,15 +4,12 @@ import cross from './assets/images/icon-cross.svg'
 
 export default function Note(props){
 
-    const [note,setNote]=useState({id:"",
-                                  body:"",
-                                   isSelected : false})
     return (
         <div className={props.darkMode?"dark card":"card"}>
             <button onClick={()=>props.handleCheckbox(props.id)} className={props.isSelected? "checkbox-btn selected":"checkbox-btn"} ></button>
             
            <p className={props.isSelected? "note-body-selected":""}>{props.body}</p> 
            <img onClick={()=> props.deleteNote(props.id)} src={cross} className="cross-img"></img>
-            </div>
+        </div>
     )
 }
